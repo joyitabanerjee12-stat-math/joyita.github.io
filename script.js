a63e2d9c08b001ac878c5f3af79df7d7c819df1b
Joyita Banerjee Ganguly <joyitabanerjee12@gmail.com>
2025-11-18 14:38:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("modeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+        toggleBtn.textContent = "🌙 Dark Mode";
+    }
+});
